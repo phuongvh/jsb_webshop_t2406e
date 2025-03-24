78a8347b8e1345bef02ee5065453a444be21ef9d
phuongvh <anhgiaotiep@gmail.com>
2025-03-24 21:16:09 +0700

    Đã hoàn tất mvc Form Manufacturer

--- src/main/java/exam/jsb_webshop_t2406e/domain/manufacturer/AdminManufacturerController.java
+++ src/main/java/exam/jsb_webshop_t2406e/domain/manufacturer/AdminManufacturerController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -50,4 +52,30 @@ public class AdminManufacturerController
         // ...được đặt vào bố cục chung của toàn website
         return "layout/layout-admin.html"; 
     }
-}
+
+    
+    @PostMapping("/admin/manufacturer/add")
+    public String postAdd(@ModelAttribute Manufacturer entity) 
+    {
+
+        // Một số thông tin thêm mới do hệ thống làm
+        // bên cạnh dữ liệu mà user gõ trên Form Add New
+        // dl.setNgayTao(LocalDate.now());
+        // dl.setNgayedit(LocalDate.now());
+
+        // Bản ghi mới mà Html Form submit lên, không có thời gian hiện tại
+        // Nên chỗ này phải làm thủ công: Gán nhãn thời gian cho thực thể mới...
+        // Chú Ý: không lấy thông tin này bên view HTMl vì sẽ có độ trễ lớn, thậm chí bị hack
+        entity.setDateCreated(new java.sql.Date(System.currentTimeMillis()));
+        entity.setDateUpdated(new java.sql.Date(System.currentTimeMillis()));
+        
+        // ...rồi mới lưu thực thể mới vào csdl
+        jpaManufacturer.save(entity);
+
+        // Gửi thông báo thành công từ giao diện Thêm Mới sang giao diện Duyệt
+        session.setAttribute("SUCCESS_MESSAGE", "Successfully added new user !");
+
+        // return "redirect:/admin/manufacturer/list";
+        return "redirect:/app/test";
+    }
+}// end class
